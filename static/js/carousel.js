2e32e6a5b2d30ec18faa2583281ac714e9dc6377
J0hn1975 <johnwrafter@live.co.uk>
2021-11-14 18:48:23 +0000

    Add carousel, images, changed text colour from default to white

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,4 +1,4 @@
-var myCarousel = document.querySelector('#myCarousel')
+var myCarousel = document.querySelector('carouselExampleDark')
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 5000,
   wrap : true
