c97ce0f232b634299d80e2486ebdb2bb0b8ebaf6
J0hn1975 <johnwrafter@live.co.uk>
2021-11-25 19:01:00 +0000

    Added checkout views and templates

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 # Register your models here.
+
+
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
