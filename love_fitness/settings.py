3d403305dd8650d4c5099e3afe6fd61e486053dd
J0hn1975 <johnwrafter@live.co.uk>
2021-12-07 16:05:18 +0000

    Changed static file settings in an attempt to resolve Amazon AWS static files issue

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -156,8 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 's3://ci-ms4-lovefitness/static/'
-STATIC_URL = 's3://ci-ms4-lovefitness/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
