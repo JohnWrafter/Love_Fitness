d4c2391b1ffe64300e6f04dd09d81fce12204dc5
J0hn1975 <johnwrafter@live.co.uk>
2021-12-08 14:07:04 +0000

    Cleaned up corrupt requirements.txt file

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -15,6 +15,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'test')
 DEBUG = True
 ALLOWED_HOSTS = ['ci-ms4-lovefitness.herokuapp.com', 'localhost']
 
+CSRF_TRUSTED_ORIGINS = ["https://8000-beige-snake-abvt0s2z.ws-eu21.gitpod.io/"]
+
+
 
 # Application definition
 
@@ -156,8 +159,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ staticfiles/js/carousel.js
@@ -0,0 +1,5 @@
+var myCarousel = document.querySelector('carouselExampleDark')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 5000,
+  wrap : true
+})
