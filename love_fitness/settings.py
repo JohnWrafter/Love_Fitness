16b9c3026f3e6b8d27b1c43bf985dfe2e5aea2c6
J0hn1975 <johnwrafter@live.co.uk>
2021-12-10 12:23:30 +0000

    Added DEVELOPMENT = True

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -168,6 +168,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = 'staticfiles'
+DEVELOPMENT = True
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
