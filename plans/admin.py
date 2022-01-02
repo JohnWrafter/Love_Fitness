80e4fe87abf81a716836dcfbfc80fca0276fcdfc
J0hn1975 <johnwrafter@live.co.uk>
2022-01-02 11:42:37 +0000

    Registered models

--- plans/admin.py
+++ plans/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Exercise, Nutrition
 
 # Register your models here.
+
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'plan',
+        'friendly_name',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('name',)
+
+
+class NutritionAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'plan',
+        'friendly_name',
+        'price',
+        'rating',
+        'image',
+    )
+
+
+admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(Nutrition, NutritionAdmin)
