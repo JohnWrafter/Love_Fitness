9a46da3b8d6606110a4d8ee2423b697971340537
J0hn1975 <johnwrafter@live.co.uk>
2022-01-02 16:30:00 +0000

    Added Plans Details views

--- plans/views.py
+++ plans/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+
+from .models import Exercise, Nutrition
 
 # Create your views here.
 
@@ -7,3 +9,15 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'plans/plans.html')
+
+
+def plans_detail(request, plan_id):
+    """ A view to show individual plan details """
+
+    plan = get_object_or_404(Plan, pk=plan_id)
+
+    context = {
+        'plan': plan,
+    }
+
+    return render(request, 'plans/plans_detail.html', context)
