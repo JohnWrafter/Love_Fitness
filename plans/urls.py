80c04ce9054956eefd0d2015acaeead51a00a8b1
J0hn1975 <johnwrafter@live.co.uk>
2022-01-26 16:14:23 +0000

    Removed Plans as no longer required

--- plans/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.plans, name='plans'),
-    path('<int:plan_id>/', views.plans_detail, name='plans_detail'),
-]
