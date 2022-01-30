2968bff43029811a0f2c1f96687c0d19147fa9ff
J0hn1975 <johnwrafter@live.co.uk>
2022-01-30 16:58:29 +0000

    Modified wish list

---
+++ wishlist/contexts.py
@@ -0,0 +1,32 @@
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+# Internal:
+from .models import Favourites
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+def wishlist_contents(request):
+    """
+    A context for Favourites to display count in users navbar
+    Args:
+        request (object): HTTP request object.
+    Returns:
+        context: context
+    """
+    try:
+        favourites = get_object_or_404(Favourites, username=request.user.id)
+        favourites_items = favourites.products.all()
+        favourites_count = len(favourites_items)
+    except Http404:
+        favourites_count = None
+
+    context = {
+        'favourites_count': favourites_count,
+    }
+
+    return context
