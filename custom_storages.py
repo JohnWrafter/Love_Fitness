003138658a0308910b35973dbcdbc666fc41614e
J0hn1975 <johnwrafter@live.co.uk>
2022-06-28 18:27:01 +0000

    Added storage locations

--- custom_storages.py
+++ custom_storages.py
@@ -1,9 +1,11 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-
-# class StaticStorage(S3Boto3Storage):
-#     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    bucket_name = 'ci-ms4-lovefitness'
+    location = 'media'
+
+
+class StaticStorage(S3Boto3Storage):
+    bucket_name = 'ci-ms4-lovefitness'
+    location = 'static'
