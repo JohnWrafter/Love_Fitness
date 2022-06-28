4b244f1eb257a92f8807eb50268bb024d3ccfcc6
J0hn1975 <johnwrafter@live.co.uk>
2022-06-28 13:38:57 +0000

    Added media file storage location

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -207,6 +207,8 @@ if 'USE_AWS' in os.environ:
     STATICFILESTATICFILES_LOCATION = 'static'
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     MEDIAFILES_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
