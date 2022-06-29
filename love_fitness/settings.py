2b3b47f95fb3808eed0bd3e039cce03e4818a6fe
J0hn1975 <johnwrafter@live.co.uk>
2022-06-29 14:16:59 +0000

    Installed gunicorn

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -6,9 +6,16 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 import os
 import dj_database_url
+from pathlib import Path
 
+
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
