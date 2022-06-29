e145227695e4e71ab89dba15d558cc05db4b594a
J0hn1975 <johnwrafter@live.co.uk>
2022-06-29 12:54:51 +0000

    Added Cloudinary storage settings

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -13,6 +13,10 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+CLOUDINARY_STORAGE = {
+    # other settings, like credentials
+    'STATICFILES_MANIFEST_ROOT': os.path.join(BASE_DIR, 'my-manifest-directory')
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
