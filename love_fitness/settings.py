2cd8004023d38ae5cb348feba722d5372c537b88
J0hn1975 <johnwrafter@live.co.uk>
2022-06-29 14:06:16 +0000

    Removed cloudinary

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -13,10 +13,10 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-CLOUDINARY_STORAGE = {
-    # other settings, like credentials
-    'STATICFILES_MANIFEST_ROOT': os.path.join(BASE_DIR, 'my-manifest-directory')
-}
+# CLOUDINARY_STORAGE = {
+#     # other settings, like credentials
+#     'STATICFILES_MANIFEST_ROOT': os.path.join(BASE_DIR, 'my-manifest-directory')
+# }
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -59,10 +59,7 @@ INSTALLED_APPS = [
     'blog',
     'wishlist',
     'contact',
-    'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary'
-
+    
     # Other
     'crispy_forms',
     'storages',
