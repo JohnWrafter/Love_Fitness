66630f43475d1f880383b657a3bb82f83654e5c6
J0hn1975 <johnwrafter@live.co.uk>
2022-09-30 18:27:43 +0000

    Added app name as a module

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -45,6 +45,7 @@ CSRF_TRUSTED_ORIGINS = ["https://8000-j0hn1975-cims4lovefitne-n0wabjpcq9h.ws-eu4
 # Application definition
 
 INSTALLED_APPS = [
+    'love-fitness.app.love-fitnessConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
