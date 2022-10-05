ca6750a1c3221cad523de3c03550a91988d2105f
J0hn1975 <johnwrafter@live.co.uk>
2022-10-05 09:33:59 +0000

    Changed database URL

--- love_fitness/settings.py
+++ love_fitness/settings.py
@@ -136,7 +136,7 @@ WSGI_APPLICATION = 'love_fitness.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('HEROKU_POSTGRESQL_CHARCOAL_URL'))
     }
 else:
     DATABASES = {
