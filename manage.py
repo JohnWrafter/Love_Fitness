833539915efacf8c12172f83f865af7e060b1aae
J0hn1975 <johnwrafter@live.co.uk>
2022-12-15 20:03:43 +0000

    Add dotenv

--- manage.py
+++ manage.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+import dotenv
+
+
+if __name__ == "__main__":
+    dotenv.read_dotenv()
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'love_fitness.settings')
