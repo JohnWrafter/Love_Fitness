098726db318c2864cde7d3643fc989a36c4133e3
J0hn1975 <johnwrafter@live.co.uk>
2022-12-17 19:22:02 +0000

    Add changes

--- love_fitness/asgi.py
+++ love_fitness/asgi.py
@@ -1,16 +1,19 @@
 """
-ASGI config for love_fitness project.
-
+ASGI config for rugby_shop project.
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 import os
-
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'love_fitness.settings')
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lovefitness.settings')
 
 application = get_asgi_application()
