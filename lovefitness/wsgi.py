c22a8f44037c690c20d7c30dee0f2de929c8de88
J0hn1975 <johnwrafter@live.co.uk>
2022-12-18 17:38:43 +0000

    Changed name

--- lovefitness/wsgi.py
+++ lovefitness/wsgi.py
@@ -16,5 +16,4 @@ from django.core.wsgi import get_wsgi_application
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lovefitness.settings')
-
 application = get_wsgi_application()
