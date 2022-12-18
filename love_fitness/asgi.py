45870b99b1d2ce7cf47d000218f151895c0a974c
J0hn1975 <johnwrafter@live.co.uk>
2022-12-18 18:11:39 +0000

    Changes

--- love_fitness/asgi.py
+++ love_fitness/asgi.py
@@ -14,6 +14,6 @@ from django.core.asgi import get_asgi_application
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lovefitness.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lovefitness.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
