89e693d6aa7ed99f25fd59427dca26b7ef9725ce
J0hn1975 <johnwrafter@live.co.uk>
2023-03-09 23:01:23 +0000

    Add corsheaders

--- lovefitness/settings.py
+++ lovefitness/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'blog',
     'wishlist',
     'contact',
+    'corsheaders',
 
     # Other
     'crispy_forms',
@@ -68,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
    ]
 
 ROOT_URLCONF = 'lovefitness.urls'
