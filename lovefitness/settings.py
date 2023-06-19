0149e6447bc6a9b5c526b026d2eca81a20dffe27
J0hn1975 <johnwrafter@live.co.uk>
2023-06-19 19:14:04 +0000

    Changed allowed hosts

--- lovefitness/settings.py
+++ lovefitness/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-johnwrafter-lovefitness-srev4lukvwh.ws-eu100.gitpod.io', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
