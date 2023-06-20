93c0d296d1f7d2ea83927e7f4d937d151d2878a5
J0hn1975 <johnwrafter@live.co.uk>
2023-06-20 16:13:38 +0000

    Set Debug to development

--- lovefitness/settings.py
+++ lovefitness/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-johnwrafter-lovefitness-srev4lukvwh.ws-eu99.gitpod.io', 'localhost', '127.0.0.1']
 
