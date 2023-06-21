bc2b2ce0abcd185a4a11970558662ba0b133cf4d
J0hn1975 <johnwrafter@live.co.uk>
2023-06-21 15:51:11 +0000

    Updates allowed hosts

--- lovefitness/settings.py
+++ lovefitness/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['8000-johnwrafter-lovefitness-36heizmjpuy.ws-eu100.gitpod.io']
 
 # Application definition
 
