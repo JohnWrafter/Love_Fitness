b8a9a896ebbacb0a3c0e21396d6bdee1cb56ce39
John Wrafter <johnwrafter@live.co.uk>
2023-06-21 18:41:13 +0100

    Added secret key

--- lovefitness/settings.py
+++ lovefitness/settings.py
@@ -29,11 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '6IQ>Ovd&_1b<DRL6ctOne0yjED')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-johnwrafter-lovefitness-36heizmjpuy.ws-eu100.gitpod.io','127.0.01']
+ALLOWED_HOSTS = ['8000-johnwrafter-lovefitness-36heizmjpuy.ws-eu100.gitpod.io', '127.0.01']
 
 # Application definition
 
