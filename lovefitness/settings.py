27132fc90a4912ef7e6c60d06f64878cc098a379
John Wrafter <johnwrafter@live.co.uk>
2023-06-23 17:17:23 +0100

    update

--- lovefitness/settings.py
+++ lovefitness/settings.py
@@ -200,7 +200,6 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
