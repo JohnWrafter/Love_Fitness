cc27e3aefa7305720052e7b5e6ba3b14af0034e6
John Wrafter <johnwrafter@live.co.uk>
2023-06-23 15:28:32 +0100

    Add media location

--- lovefitness/settings.py
+++ lovefitness/settings.py
@@ -228,8 +228,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
-    STATIC_URL = 'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = 'https://lovefitness.s3.eu-west-2.amazonaws.com/media/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 FREE_DELIVERY_THRESHOLD = 99
